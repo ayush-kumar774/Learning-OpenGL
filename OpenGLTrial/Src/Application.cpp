1906873dce633f2947b4a97914cdd3208dc6956b
ayush-kumar774 <ayushkumar1234@protonmail.com>
2020-11-07 07:37:20 +0530

    Rendered Orange Triangle

--- OpenGLTrial/Src/Application.cpp
+++ OpenGLTrial/Src/Application.cpp
@@ -32,6 +32,8 @@ int main(void)
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         
         createTriangle();
